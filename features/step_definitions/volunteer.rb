106137b0d069d179bc6647cd180707c6866790ef
Adam Krawiec <adamkrawiec@gmail.com>
2012-09-20 19:53:19 -0500

    adding features

---
+++ features/step_definitions/volunteer.rb
@@ -0,0 +1,21 @@
+# volunteer.rb
+
+Given /^a new volunteer shows to the shop$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^they visit the website$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the they enter their email address$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^click "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^they will see "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/volunteer.feature
@@ -0,0 +1,18 @@
+# volunteer.feature
+Feature: Handle volunteer actions
+  In order to value
+  As a role
+  I want feature
+
+  Scenario: New Volunteer shows up at shop
+      Given a volunteer shows to the shop
+      When they visit the website
+      And the they enter their email address
+      And click "login"
+      Then they will see "Welcome new volunteer!"
+    
+    
+      
+    
+  
+  
