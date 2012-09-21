8b66b3a2af55c04eec32d6ba0efa8edab38814bb
Adam Krawiec <adamkrawiec@gmail.com>
2012-09-20 19:15:41 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cbs::Application.config.secret_token = '2bd38c8a3d11a4eacd287f9f6c22ac6320547871827f0d01fdeff21aeb746ae8c356fbd0e4cdec7caf37d80da35bc08b3ca815fb6dbe96f85d5c68d19858faa0'
