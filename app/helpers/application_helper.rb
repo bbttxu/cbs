9820e5bdb8b7d985eb562bd570b9e099cd1111b2
bbttxu <bbttxu@gmail.com>
2012-10-07 16:42:16 -0500

    putting all (i hope) views into grid

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def li_link_to(text, url, options = nil)
+		link_to_unless_current text, url, options do |text, url, options|
+			Haml::Engine.new("%li.active= link_to(#{text}, #{url}, #{options})").render
+		end
+	end
+
+	# chronic will parse this no problem
+	def standard_datetime( data )
+		data.strftime('%Y-%m-%d %H:%M')
+	end
 end
