a9320056aa0e4a404baeaf4aca19ae57969334ef
Adam Krawiec <adamkrawiec@gmail.com>
2012-12-11 15:02:27 -0600

    http://stackoverflow.com/questions/10826183/syntax-error-when-trying-to-start-rails-server

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -5,7 +5,9 @@
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
 ActiveSupport.on_load(:action_controller) do
-  wrap_parameters format: [:json]
+  # FIXME don't need in ruby 1.9
+  # http://stackoverflow.com/questions/10826183/syntax-error-when-trying-to-start-rails-server
+  wrap_parameters  :format => [:json]
 end
 
 # Disable root element in JSON by default.
