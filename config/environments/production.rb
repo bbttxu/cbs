d984f17ff04e79f33894b9b7aaa5042befede636
Adam Krawiec <adamkrawiec@gmail.com>
2013-02-05 13:48:59 -0600

    adding FIXME tag to some code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Cbs::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
+  # FIXME
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
