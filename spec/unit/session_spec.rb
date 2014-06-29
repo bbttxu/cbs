284cb20b6158858fa5ef3b87b64c1390c2bf6012
Adam Krawiec <adamkrawiec@gmail.com>
2014-06-29 12:32:38 -0500

    overriding travis test script

--- spec/unit/session_spec.rb
+++ spec/unit/session_spec.rb
@@ -1,8 +1,8 @@
-require 'test_helper'
+require 'spec_helper'
 
 describe "#new" do
-    # it "takes three parameters and returns a Book object" do
-    #     @book.should be_an_instance_of Book
-    # end
+    it "takes three parameters and returns a Book object" do
+        # @book.should be_an_instance_of Book
+    end
 end
     
