7acc5aa993285b65a6f887670dda171f80e61572
Adam Krawiec <adamkrawiec@gmail.com>
2014-07-07 18:35:31 -0500

    adding new file [skip ci]

---
+++ spec/integration/shops_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe ShopsController do
+
+  describe "GET current shops" do
+    it "assigns @shops" do
+      shops = FactoryGirl.create(:shop)
+
+      get 'shops/current'
+      # FIXME
+      # expect(assigns(:shops).all).to eq([shops.to_json])
+    end
+
+    it "renders the index template" do
+      get 'shops/current'
+      expect(response).to render_template("current")
+    end
+  end
+end
