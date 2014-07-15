245e01c4d0b0590dbf8b76754037064c7ac1c094
Adam Krawiec <adamkrawiec@gmail.com>
2014-07-14 19:42:15 -0500

    spaces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD'], except: [ :current ]
+  http_basic_authenticate_with name: ENV['admin_user'], password: ENV['admin_password'], except: [ :current ]
 
 end
