0129ea7665a839cf9df90e11421504218a0309a7
Adam Krawiec <adamkrawiec@gmail.com>
2014-09-17 18:58:58 -0500

    adding failing tests

--- spec/unit/volunteer_spec.rb
+++ spec/unit/volunteer_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'volunteers' do |variable|
-	describe "modle" do
+	describe "model" do
 		it "should be valid" do
 			volunteer = FactoryGirl.build 'volunteer'
 			volunteer.should be_valid
@@ -22,6 +22,23 @@ describe 'volunteers' do |variable|
     it 'should be possible to be on a mailing list' do
       volunteer = FactoryGirl.build 'volunteer', can_email: true
       volunteer.can_email.should be(true)
+    end
+  end
+
+  describe 'last date of activity' do
+    it 'should be zero if volunteer has worked zero shops' do
+      volunteer = FactoryGirl.build 'volunteer'
+      volunteer.last_active.should be(nil)
+    end
+
+    it 'should provide date of last activity' do
+      date = Chronic.parse("Jul 1, 1978, 3pm")
+
+      volunteer = FactoryGirl.create 'volunteer'
+      # shop = FactoryGirl.build 'shop', starts_at: Chronic.parse("Jul 1, 1978, 3pm")
+      session = FactoryGirl.create 'session', starts_at: date
+      volunteer.sessions << session
+      volunteer.last_active.should be(date)
     end
 	end
 end
